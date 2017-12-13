9c08c3fa52fd1069b8429a9cfcb6fa736d6315e5
bantohin <dimitartachev23@gmail.com>
2017-12-13 18:21:04 +0200

    Initial Commit

---
+++ HairConnect.Data/HairConnectDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace HairConnect.Data
+{
+    using Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    
+    public class HairConnectDbContext : IdentityDbContext<User>
+    {
+        public HairConnectDbContext(DbContextOptions<HairConnectDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ HairConnect.Web/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace HairConnect.Web.Infrastructure.Extensions
+{
+    using Data;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseDatabaseMigration(this IApplicationBuilder app)
+        {
+            using (IServiceScope serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<HairConnectDbContext>().Database.Migrate();
+            }
+
+            return app;
+        }
+    }
+}
